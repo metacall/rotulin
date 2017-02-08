382521cd009c0f904d9d5900e14e52afaca5eaa7
loopzer <loopzer@gmail.com>
2017-02-08 14:12:16 +0100

    Added Image project

---
+++ image/ImageMaker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace ImageLib
+{
+    public class ImageMaker
+    {
+        public static string MakeImage(string text)
+        {
+            using (Bitmap img = new Bitmap(100, 50))
+            {
+                using (Graphics grh = Graphics.FromImage(img))
+                {
+                    using (var font = new Font("Arial", 8))
+                    {
+                        grh.DrawString(text, font, Brushes.Red, 0, 0);
+                    }
+                }
+                var fileName = System.IO.Path.GetRandomFileName() + ".png";
+
+                img.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ image/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ImageLib
+{
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            ImageLib.ImageMaker.MakeImage("hola");
+        }
+    }
+}
