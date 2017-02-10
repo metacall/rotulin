8dbc44ce88d4d36a1e5a348dcebdce2fa2d14486
Vicente Ferrer <vic798@gmail.com>
2017-02-10 03:59:02 +0100

    Added make command to configure sh script.

--- configure.bat
+++ configure.bat
@@ -4,5 +4,10 @@ if not exist "build" mkdir build
 cd build
 cmake ..
 
+REM nmake
+
+echo.
+echo.
+
 echo "Copy your lib*_loader[d].dll libraries to output path"
 echo "Copy your lib*_config[d].dll libraries to output/configurations path"
--- configure.sh
+++ configure.sh
@@ -3,6 +3,10 @@
 mkdir -p build
 cd build
 cmake ..
+make
+
+echo
+echo
 
 echo "Copy your lib*_loader[d].so libraries to output path"
 echo "Copy your lib*_config[d].so libraries to output/configurations path"
