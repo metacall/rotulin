796c0aea128d72aa47e22144a1569887f4cd0fc0
Vicente Ferrer <vic798@gmail.com>
2017-02-10 03:36:36 +0100

    Added metacall in cache (not working, when loading a module, script file is locked).

--- cache/cache.rb
+++ cache/cache.rb
@@ -1,25 +1,41 @@
-@@dic={}
+#require_relative 'rb_portd'
+
+@dic={}
 
 def cache_initialize()
-	@@dic={}
+	@dic={}
 
 	puts 'ruby: initialize'
+
+	#Rb_portd.metacall_load_from_file('py', [ 'frontend.py' ])
+	#result = Rb_portd.metacall('frontend_initialize', 9000)
+	#puts result
+
+	#result = Rb_portd.metacall('complex_algorithm', 'hello world')
+	#puts result
+
 end
 
 def cache_has_key(key: String)
+
 	puts 'ruby: has key'
 
-    return @@dic.has_key? key
+	return @dic.has_key? key
+
 end
 
 def cache_set(key: String, value: String)
+
 	puts 'ruby: set value'
 
-    @@dic[key] = value
+	@dic[key] = value
+
 end
 
 def cache_get(key: String)
+
 	puts 'ruby: get value'
 
-    @@dic[key]
+	@dic[key]
+
 end
